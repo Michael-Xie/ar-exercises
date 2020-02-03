54e39b1fb7d58ddd8da23c741e41e15aa1fbd145
Michael-Xie <xiemicha@gmail.com>
2020-02-03 23:13:43 +0000

    Loaded a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,18 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true, mens_apparel: false)
+Store.create(name: "Whistler", annual_revenue: 1900000, womens_apparel: false, mens_apparel: true)
+Store.create(name: "Yaletown", annual_revenue: 430000, womens_apparel: true, mens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true);
+
+for store in @mens_stores
+  puts "#{store.name} #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000);
+
+for store in @womens_stores
+  puts "#{store.name} #{store.annual_revenue}"
+end
