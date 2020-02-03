04fb119dfd0122abfee781e74d658ab056c2b07a
Michael-Xie <xiemicha@gmail.com>
2020-02-03 23:39:10 +0000

    Made one to many associations between employee and store
    Added employees to stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Emp 2", last_name: "Virani", hourly_rate: 20)
+@store2.employees.create(first_name: "Emp 3", last_name: "Virani", hourly_rate: 30)
+@store2.employees.create(first_name: "Emp 4", last_name: "Virani", hourly_rate: 40)
