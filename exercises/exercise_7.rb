9eca7623ab7241c5e02dd47e69e17413a45796a4
Michael-Xie <xiemicha@gmail.com>
2020-02-04 17:27:20 +0000

    Added validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+# Ask the user for a store name (store it in a variable)
+# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+employee1 = @store2.employees.create(last_name: "Virani", hourly_rate: "string")
+employee2 = @store2.employees.create(first_name: "Emp 5", hourly_rate: 300)
+employee3 = Employee.create(first_name: "No", last_name: "Store", hourly_rate: 30)
+p employee1.errors.messages
+p employee2.errors.messages
+p employee3.errors.messages
+
+print "Enter a store name: "
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name, womens_apparel: false)
+p new_store.errors.messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :store_must_carry_one_apparel
+  def store_must_carry_one_apparel
+    puts mens_apparel
+    puts womens_apparel
+    if !mens_apparel && !womens_apparel
+      errors[:base] << "must have either men's apparel or women's apparel"
+    end
+  end
 end
+
+# Stores must always have a name that is a minimum of 3 characters
+# Stores have an annual_revenue that is a number (integer) that must be 0 or more
